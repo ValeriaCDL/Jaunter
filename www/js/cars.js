e89d8d53178d98d74ca93e395f0e6a87ba462f77
Ricardo Velazquez <ricardo.velazquez.serna@hotmail.com>
2017-03-15 18:50:25 -0700

    validaciones en los campos de crear carro

--- www/js/cars.js
+++ www/js/cars.js
@@ -2,6 +2,7 @@
 angular.module('jaunter.cars', ['jaunter.shared'])
 
 .controller('CarsCtrl', function($scope,CarSvc) {
+
   CarSvc.All().then(function(c) {
     $scope.Cars = c;
   });
@@ -9,28 +10,91 @@ angular.module('jaunter.cars', ['jaunter.shared'])
 .controller('CarCtrl', function($scope,$stateParams,CarSvc) {
   //$scope.car = angular.fromJson($stateParams.car); //funciona..pero noseqpedo con la vista
   $scope.car = CarSvc.Get($stateParams.carId);
+
 })
 
-.controller('CarFormCtrl', function($scope, $http,Constants){
+.controller('CarFormCtrl', function($scope, $http,Constants,CarValidationFactory){
   // POR ahora casi todos los campos son obligatorios...
+  $scope.hidden=true;
+  $scope.checkInput = function(event){
+
+    var input = document.getElementById(event.target.id).value;
+    // console.log(event.target.id);
+    var regStr = /^([A-z]{2,15})$/i;
+    var regNum = /^([0-9]{3,4})$/i;
+    switch (event.target.id) {
+      case 'brandInput':
+      if(regStr.test(input)) {
+        CarValidationFactory.brand=true;
+        // console.log('Brand'+' Paso');
+      }
+      break;
+      case 'modelInput':
+      if(regStr.test(input)) {
+        CarValidationFactory.model=true;
+        // console.log('Model'+' Paso');
+      }
+      break;
+      case 'yearInput':
+      if(input!='') {
+        CarValidationFactory.year=true;
+        // console.log('Year'+' Paso');
+      }
+      break;
+      case 'colorInput':
+      if(input!='') {
+        CarValidationFactory.color=true;
+        // console.log('Color'+' Paso');
+      }
+      break;
+      case 'seatInput':
+      if(input!='') {
+        CarValidationFactory.seat=true;
+        // console.log('Seat'+' Paso');
+      }
+      break;
+      case 'descriptionInput':
+      if(input!='') {
+        CarValidationFactory.description=true;
+        // console.log('description'+' Paso');
+      }
+      break;
+      default:
+
+    }
+    if(CarValidationFactory.brand==true && CarValidationFactory.model==true && CarValidationFactory.year==true && CarValidationFactory.year==true && CarValidationFactory.seat==true && CarValidationFactory.description==true){
+      $scope.hidden =false;
+    }
+
+  }
   $scope.post= function(obj){
     $http.post(Constants.BaseUrl+"Autos?access_token="+Constants.TemporalToken,
-     obj).then(function(response){
-       console.log(response);
-     });
-  };
+    obj).then(function(response){
+      console.log(response);
+    });
+  }
 })
-
+.factory('CarValidationFactory', function(){
+  var valid = {
+    brand:false,
+    model:false,
+    year:false,
+    color:false,
+    seat:false,
+    description:false
+  };
+  return valid;
+})
 .factory('CarSvc',function($http,Constants){
   var url =Constants.BaseUrl;
   var cars;
   return {
     All: function() {
-        return $http.get(url+"Autos?access_token="+Constants.TemporalToken)
-         .then(function(response){
-           cars = response.data;
-           return cars;
-         });
+      return $http.get(url+"Autos?access_token="+Constants.TemporalToken)
+      .then(function(response){
+        cars = response.data;
+        return cars;
+      });
     },
     Get: function(carId) {
       ///Esto es propenso a error porque primero tengo q haber hecho el All
